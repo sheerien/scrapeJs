1275e6eac2cd5a2c7140a8a84e302566d89ecba6
sheerien <freemebabaj@gmail.com>
2021-06-22 11:36:25 +0300

    ADD: two methods to written oop class in js

---
+++ src/js/scrape_es6_oop.js
@@ -0,0 +1,85 @@
+export class ScrapeMovie {
+    /**
+     * 
+     * @param {String} proxy - proxy url
+     * @param {String} target - site url
+     */
+    constructor(proxy, target) {
+        this.proxy = proxy;
+        this.target = target;
+    }
+
+
+
+    /**
+     * 
+     * @param {String} proxy 
+     * @param {String} target 
+     * @returns Promises
+     */
+    getDocumentHtml(proxy, target) {
+        return new Promise((resolve, reject) => {
+            let req = new XMLHttpRequest();
+            req.open("GET", `http://${proxy}/${target}`)
+            req.responseType = "document";
+            req.onload = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    resolve(req.responseXML)
+                } else(
+                    reject(Error(this.statusText))
+                )
+            };
+            req.send()
+        });
+    }
+
+    /**
+     * 
+     * @param {String} proxy 
+     * @param {String} target 
+     * @returns Promises
+     */
+    getText(proxy, target) {
+        return new Promise((resolve, reject) => {
+            let req = new XMLHttpRequest();
+            // console.log(`http://${proxy}/${target}`);
+            req.open("GET", `http://${proxy}/${target}`)
+            req.responseType = "text";
+            req.onload = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    resolve(req.responseText)
+                } else(
+                    reject(Error(this.statusText))
+                )
+            };
+            req.send()
+        });
+    }
+
+    selectByCss(selector) {
+        return document.querySelector(selector)
+    }
+
+    /**
+     * 
+     * @param {object} document 
+     * @param {String} selector 
+     * @returns String
+     */
+
+    select(document, selector) {
+        return document.querySelector(selector)
+    }
+
+    /**
+     * 
+     * @param {object} document 
+     * @param {String} selector 
+     * @returns Array
+     */
+
+    selectAll(document, selector) {
+        return [...document.querySelectorAll(selector)]
+    }
+
+}
